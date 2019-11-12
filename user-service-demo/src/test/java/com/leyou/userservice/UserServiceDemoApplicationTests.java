0249d75f467effcf91f2ae1eea6ac645d4be5c74
wangpxp <wangpxp@gmail.com>
2019-11-12 18:57:26 +0800

    Initial commit

---
+++ user-service-demo/src/test/java/com/leyou/userservice/UserServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
